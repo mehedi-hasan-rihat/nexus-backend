ee0ccba4fa267db192a8eb1c6b433be713cfd505
Mehedi Hasan Rihat <mehedi4794@gmail.com>
2026-02-23 22:18:26 +0600

    add add-volunteer-role and better auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,41 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { UserRole } from "../generated/prisma/enums";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql"
     }),
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                required: true,
+                defaultValue: UserRole.STUDENT
+            },
+
+            isActive: {
+                type: "boolean",
+                required: true,
+                defaultValue: false
+            },
+
+            phone: {
+                type: "string",
+                required: false,
+            },
+
+            address: {
+                type: "string",
+                required: false,
+            }
+
+        }
+    },
+
 });
