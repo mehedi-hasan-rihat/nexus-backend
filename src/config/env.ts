3ec21679d20e89a6bacfd4eeae1544cd32c40310
mehedi-hasan-rihat <mehedi4794@gmail.com>
2026-04-23 18:57:58 +0600

    fix: update NODE_ENV default value to production in env configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const envVars = {
-    NODE_ENV: process.env.NODE_ENV ?? "development",
+    NODE_ENV: process.env.NODE_ENV ?? "production",
     PORT: process.env.PORT ?? "5000",
     DATABASE_URL: process.env.DATABASE_URL!,
     CLIENT_URL: process.env.CLIENT_URL ?? "http://localhost:3000",
